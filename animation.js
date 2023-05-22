ea98db69d5a97beb59a45ba0c3468be666a1620d
unwxnted <bauti0926@gmail.com>
2023-05-21 22:49:12 -0300

    fixed all, maybe finished

--- animation.js
+++ animation.js
@@ -1,16 +1,16 @@
 var canvas = document.getElementById("canvas"),
-    ctx = canvas.getContext('2d');
+  ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var stars = [],
-    FPS = 100,
-    x = 300, 
-    mouse = {
-      x: 0,
-      y: 0
-    }; 
+  FPS = 25,
+  x = 256,
+  mouse = {
+    x: 0,
+    y: 0
+  };
 
 
 
@@ -26,13 +26,13 @@ for (var i = 0; i < x; i++) {
 
 
 function draw() {
-  ctx.clearRect(0,0,canvas.width,canvas.height);
-  
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   ctx.globalCompositeOperation = "lighter";
-  
+
   for (var i = 0, x = stars.length; i < x; i++) {
     var s = stars[i];
-  
+
     ctx.fillStyle = "#fff";
     ctx.beginPath();
     ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
@@ -40,16 +40,16 @@ function draw() {
     ctx.fillStyle = 'black';
     ctx.stroke();
   }
-  
+
   ctx.beginPath();
   for (var i = 0, x = stars.length; i < x; i++) {
     var starI = stars[i];
-    ctx.moveTo(starI.x,starI.y); 
-    if(distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y);
+    ctx.moveTo(starI.x, starI.y);
+    if (distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y);
     for (var j = 0, x = stars.length; j < x; j++) {
       var starII = stars[j];
-      if(distance(starI, starII) < 150) {
-        ctx.lineTo(starII.x,starII.y); 
+      if (distance(starI, starII) < 150) {
+        ctx.lineTo(starII.x, starII.y);
       }
     }
   }
@@ -58,34 +58,43 @@ function draw() {
   ctx.stroke();
 }
 
-function distance( point1, point2 ){
+function distance(point1, point2) {
   var xs = 0;
   var ys = 0;
- 
+
   xs = point2.x - point1.x;
   xs = xs * xs;
- 
+
   ys = point2.y - point1.y;
   ys = ys * ys;
- 
-  return Math.sqrt( xs + ys );
+
+  return Math.sqrt(xs + ys);
 }
 
+let mx = false;
+let min = 15;
+let max = 25;
 
 function update() {
+  if(FPS === max)mx= true;
+  if(FPS === min) mx = false;
+
+  if(mx)FPS = min;
+  else FPS = max;
+  
   for (var i = 0, x = stars.length; i < x; i++) {
     var s = stars[i];
-  
+
     s.x += s.vx / FPS;
     s.y += s.vy / FPS;
-    s.x+= 1/FPS;
-    s.y+= 1/FPS;
+    s.x += 1 / FPS;
+    s.y += 1 / FPS;
     if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
     if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
   }
 }
 
-canvas.addEventListener('mousemove', function(e){
+canvas.addEventListener('mousemove', function (e) {
   mouse.x = e.clientX;
   mouse.y = e.clientY;
 });
--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-
 const squares = document.querySelectorAll('.square');
 
 let board = ['', '', '', '', '', '', '', '', ''];
 
-
 let playerSymbol = 'X';
 let currentPlayer = 'X';
-
 let aiSymbol = 'O';
 
+const button = document.createElement('button');
+button.innerHTML = "RESTART";
+button.addEventListener('click', resetGame);
+const resetContainer = document.getElementById('reset-container');
+resetContainer.appendChild(button);
 
 const winningCombinations = [
   [0, 1, 2],
@@ -21,15 +23,24 @@ const winningCombinations = [
   [2, 4, 6],
 ];
 
-function isGameOver(board) {
+function resetGame(){
+  board = ['', '', '', '', '', '', '', '', ''];
+  playerSymbol = 'X';
+  currentPlayer = 'X';
+  aiSymbol = 'O';
+  squares.forEach((square) => {
+    square.classList.remove('X', 'O', 'winning');
+    square.innerHTML = '';
+  });
+}
 
+function isGameOver(board) {
   for (let i = 0; i < winningCombinations.length; i++) {
     const [a, b, c] = winningCombinations[i];
     if (board[a] && board[a] === board[b] && board[a] === board[c]) {
       return true;
     }
   }
-
 
   if (board.includes('')) {
     return false;
@@ -38,27 +49,22 @@ function isGameOver(board) {
   return true;
 }
 
-
 function getBestMove(board, depth, maximizingPlayer) {
-
   if (isGameOver(board)) {
-
     if (!board.includes('')) {
       return 0;
     }
-
     if (maximizingPlayer) {
-      return 1;
+      return -1;
     }
-
-    return -1;
+    return 1;
   }
 
+  let bestScore;
+  let bestMove;
 
   if (maximizingPlayer) {
-    let bestScore = -Infinity;
-    let bestMove = null;
-
+    bestScore = -Infinity;
     for (let i = 0; i < board.length; i++) {
       if (board[i] === '') {
         board[i] = aiSymbol;
@@ -70,19 +76,8 @@ function getBestMove(board, depth, maximizingPlayer) {
         }
       }
     }
-
-    if (depth === 0) {
-      squares[bestMove].classList.add('ai');
-      board[bestMove] = aiSymbol;
-    }
-
-    return bestScore;
-  }
-
-  else {
-    let bestScore = Infinity;
-    let bestMove = null;
-
+  } else {
+    bestScore = Infinity;
     for (let i = 0; i < board.length; i++) {
       if (board[i] === '') {
         board[i] = playerSymbol;
@@ -94,24 +89,24 @@ function getBestMove(board, depth, maximizingPlayer) {
         }
       }
     }
-
-    if (depth === 0) {
-      squares[bestMove].classList.add('player');
-      board[bestMove] = playerSymbol;
-    }
-
-    return bestScore;
   }
+
+  if (depth === 0) {
+    return bestMove;
+  }
+
+  return bestScore;
 }
 
+
 squares.forEach((square) => {
-  square.addEventListener("click", handleClick);
+  square.addEventListener('click', handleClick);
 });
 
 function handleClick(event) {
   const square = event.target;
   const squareIndex = square.id;
-  let gameOver = isGameOver(squareIndex);
+  let gameOver = isGameOver(board);
 
   if (board[squareIndex] || gameOver) {
     return;
@@ -121,124 +116,90 @@ function handleClick(event) {
   square.classList.add(currentPlayer);
   square.innerHTML = currentPlayer;
 
-  if (checkGameOver()) {
-    gameOver = true;
+  if(isGameOver(board)) {
+    checkGameOver();
     return;
   }
 
-  currentPlayer = currentPlayer === "X" ? "O" : "X";
+  currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
 
-  if (currentPlayer === "O") {
-    //makeComputerMoveMinMax();
-    setTimeout(() => {
-      squares[makeComputerMoveTEST1()].click();
-    }, 500);
+  if (currentPlayer === 'O') {
+    makeComputerMoveMinMax();
+  }
+
+  if(isGameOver(board)) {
+    checkGameOver();
   }
 }
 
 
-function makeComputerMoveTEST1() {
-
-  let tBoard = board.slice();
-  for (let i = 0; i < board.length; i++) {
-    if (board[i] == '') {
-      tBoard[i] = 'O';
-      if (isGameOver(tBoard)) {
-        return i;
-      } else {
-        tBoard[i] = '';
-      }
-    }
-  }
-
-  for (let i = 0; i < board.length; i++) {
-    if (board[i] == '') {
-      tBoard[i] = 'X';
-      if (isGameOver(tBoard)) {
-        return i;
-      } else {
-        tBoard[i] = '';
-      }
-    }
-  }
-
-  if (board[4] == '') {
-    return 4;
-  }
-
-  if(board[4] == 'O') {
-
-    for(let i = 0; i < board.length; i++) {
-      if (i == 1 || i == 3 || i == 5 || i == 7) {
-        if (board[i] == '') {
-          return i;
-        }
-      }
-    }
-
-  }else{
-    for (let i = 0; i < board.length; i++) {
-      if (i == 0 || i == 2 || i == 6 || i == 8) {
-        if (board[i] == '') {
-          return i;
-        }
-      }
-    }
-  }
-
-  for (let i = 0; i < board.length; i++) {
-    if (board[i] == "") {
-      return i;
-    }
-  }
-
-  return -1;
-}
 
 function checkGameOver() {
   for (let i = 0; i < winningCombinations.length; i++) {
     const [a, b, c] = winningCombinations[i];
     if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-      squares[a].classList.add("winning");
-      squares[b].classList.add("winning");
-      squares[c].classList.add("winning");
+      squares[a].classList.add('winning');
+      squares[b].classList.add('winning');
+      squares[c].classList.add('winning');
 
       alert(`${board[a]} wins!`);
       return true;
     }
   }
 
-  if (!board.includes("")) {
-    alert("Tie game!");
+  if (!board.includes('')) {
+    alert('Tie game!');
     return true;
   }
 
   return false;
 }
 
-function getBestMoveIndex() {
-  let bestScore = -Infinity;
-  let bestMoveIndex;
+function checkWinnableMove(board) {
 
   for (let i = 0; i < board.length; i++) {
-    if (!board[i]) {
-      board[i] = "O";
-      const score = getBestMove(board, false);
-      board[i] = "";
-      if (score > bestScore) {
-        bestScore = score;
-        bestMoveIndex = i;
+    if (board[i] === '') {
+      board[i] = 'O';
+      if (isGameOver(board)) {
+        board[i] = '';
+        return i;
       }
+      board[i] = '';
     }
   }
 
-  return bestMoveIndex;
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === '') {
+      board[i] = 'X';
+      if (isGameOver(board)) {
+        board[i] = '';
+        return i;
+      }
+      board[i] = '';
+    }
+  }
+
+  return -1;
+}
+
+function getBestMoveIndex() {
+  const winnableMoveIndex = checkWinnableMove(board);
+  if (winnableMoveIndex !== -1) {
+    return winnableMoveIndex;
+  }
+  return getBestMove(board, 0, true);
 }
 
 function makeComputerMoveMinMax() {
   const bestMoveIndex = getBestMoveIndex();
   const bestMoveSquare = squares[bestMoveIndex];
-  setTimeout(() => {
-    bestMoveSquare.click();
-  }, 500);
+  board[bestMoveIndex] = aiSymbol;
+  bestMoveSquare.classList.add(aiSymbol);
+  bestMoveSquare.innerHTML = aiSymbol;
+
+  if (isGameOver(board)) {
+    return;
+  }
+
+  currentPlayer = playerSymbol;
 }
